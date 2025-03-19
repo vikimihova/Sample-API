fc7ae16802ccb0bab9d84324b9f7444256919a0e
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-03-19 22:50:33 +0200

    build: add DTOs

---
+++ SampleApiApp/SampleApi/Data/DTOs/CharacterDTO.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleApi.Data.DTOs
+{
+    public class CharacterDTO
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Species { get; set; } = null!;
+
+        [Required]
+        public string Gender { get; set; } = null!;
+
+        public string? House { get; set; }
+
+        public string? DateOfBirth { get; set; }
+
+        public int YearOfBirth { get; set; }
+
+        public WandDTO Wand { get; set; } = null!;
+
+        public string? Patronus { get; set; }
+    }
+}
---
+++ SampleApiApp/SampleApi/Data/DTOs/WandDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace SampleApi.Data.DTOs
+{
+    public class WandDTO
+    {    
+        public string? Wood { get; set; }
+
+        public string? Core { get; set; }
+
+        public double? Length { get; set; }
+    }
+
+}
