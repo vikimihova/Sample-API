24e80e91d5ed9ff6ff66a165ade9ef10443fbf94
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-03-19 18:00:42 +0200

    build: create extension method for adding endpoints

---
+++ SampleApiApp/SampleApi/ExtensionMethods/Endpoints/RootEndpoints.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Security;
+
+namespace SampleApi.ExtensionMethods.Endpoints
+{
+    public static class RootEndpoints
+    {
+        public static void AddRootEndpoints(this WebApplication app)
+        {
+            app.MapGet("/", () => "Hello World");
+        }
+    }
+}
--- SampleApiApp/SampleApi/Program.cs
+++ SampleApiApp/SampleApi/Program.cs
@@ -23,5 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.AddRootEndpoints();
 
 app.Run();
