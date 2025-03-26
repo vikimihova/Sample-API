5300d84d41d0619802987d3e6402f3251abf8eb9
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-03-26 16:54:33 +0200

    build: create a class Deserializer for extracting data and add it as a transient service

---
+++ SampleApiApp/SampleApi/Data/Deserializer.cs
@@ -0,0 +1,26 @@
+﻿using SampleApi.Data.DTOs;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+
+namespace SampleApi.Data
+{
+    public class Deserializer
+    {
+        private readonly List<CharacterDTO> characters;
+
+        public Deserializer()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", "Datasets", "hp-characters.json");
+            string json = File.ReadAllText(filePath);
+
+            this.characters = JsonSerializer.Deserialize<List<CharacterDTO>>(json, options) ?? new List<CharacterDTO>();
+        }
+
+        public List<CharacterDTO> Characters { get { return characters; } }
+    }
+}
--- SampleApiApp/SampleApi/Program.cs
+++ SampleApiApp/SampleApi/Program.cs
@@ -1,3 +1,4 @@
+using SampleApi.Data;
 using SampleApi.ExtensionMethods.Endpoints;
 using Scalar.AspNetCore;
 
@@ -5,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // ADD SERVICES TO THE CONTAINER
 builder.Services.AddOpenApi();
+builder.Services.AddTransient<Deserializer>();
 
 // BUILD APPLICATION
 var app = builder.Build();
