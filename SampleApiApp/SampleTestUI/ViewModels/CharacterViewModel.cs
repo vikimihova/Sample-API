883149d403fbb1908e8fcdb27406bfea74a03f59
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-03-27 15:13:49 +0200

    models: add view models

---
+++ SampleApiApp/SampleTestUI/ViewModels/CharacterViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace SampleTestUI.ViewModels
+{
+    public class CharacterViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Species { get; set; } = null!;
+
+        public string Gender { get; set; } = null!;
+
+        public string? House { get; set; }
+
+        public string? DateOfBirth { get; set; }
+
+        public int? YearOfBirth { get; set; }
+
+        public WandViewModel? Wand { get; set; }
+
+        public string? Patronus { get; set; }
+    }
+}
---
+++ SampleApiApp/SampleTestUI/ViewModels/WandViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleTestUI.ViewModels
+{
+    public class WandViewModel
+    {
+        public string? Wood { get; set; }
+
+        public string? Core { get; set; }
+
+        public double? Length { get; set; }
+    }
+}
