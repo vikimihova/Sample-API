62c1e7fa31aa7341c8566039321b6da118e1d504
Viktoriya Mihova <135457634+vikimihova@users.noreply.github.com>
2025-04-02 20:40:10 +0300

    build: add utilities folder with common output messages

---
+++ SampleApiApp/SampleApi/Utilities/OutputMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace SampleApi.Utilities
+{
+    public static class OutputMessages
+    {
+        public static class HealthChecks
+        {
+            public const string Healthy = "This is a test healthy service.";
+            public const string Unhealthy = "This is a test unhealthy service.";
+            public const string Degraded = "This is a test degraded service.";
+            public const string Random = "This is a test random service.";
+        }
+    }
+}
